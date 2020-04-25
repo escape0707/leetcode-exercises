160e01e401b5b12fa5eccd830125907397c7cb91
escape0707 <tothesong@gmail.com>
2020-04-25 21:28:07 +0800

    146

---
+++ 146.lru-cache.py
@@ -0,0 +1,85 @@
+#
+# @lc app=leetcode id=146 lang=python3
+#
+# [146] LRU Cache
+#
+# https://leetcode.com/problems/lru-cache/description/
+#
+# algorithms
+# Medium (29.77%)
+# Likes:    5218
+# Dislikes: 231
+# Total Accepted:    501.5K
+# Total Submissions: 1.6M
+# Testcase Example:  '["LRUCache","put","put","get","put","get","put","get","get","get"]\n' +
+# '[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]'
+#
+# Design and implement a data structure for Least Recently Used (LRU) cache. It
+# should support the following operations: get and put.
+#
+# get(key) - Get the value (will always be positive) of the key if the key
+# exists in the cache, otherwise return -1.
+# put(key, value) - Set or insert the value if the key is not already present.
+# When the cache reached its capacity, it should invalidate the least recently
+# used item before inserting a new item.
+#
+# The cache is initialized with a positive capacity.
+#
+# Follow up:
+# Could you do both operations in O(1) time complexity?
+#
+# Example:
+#
+#
+# LRUCache cache = new LRUCache( 2 /* capacity */ );
+#
+# cache.put(1, 1);
+# cache.put(2, 2);
+# cache.get(1);       // returns 1
+# cache.put(3, 3);    // evicts key 2
+# cache.get(2);       // returns -1 (not found)
+# cache.put(4, 4);    // evicts key 1
+# cache.get(1);       // returns -1 (not found)
+# cache.get(3);       // returns 3
+# cache.get(4);       // returns 4
+#
+#
+#
+#
+#
+
+from bisect import *
+from collections import *
+from functools import *
+from heapq import *
+from itertools import *
+from math import *
+from operator import *
+from typing import *
+
+
+# @lc code=start
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.ordered_dict: OrderedDict[int, int] = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        value = self.ordered_dict.get(key, -1)
+        if value != -1:
+            self.ordered_dict.move_to_end(key)
+        return value
+
+    def put(self, key: int, value: int) -> None:
+        self.ordered_dict[key] = value
+        self.ordered_dict.move_to_end(key)
+        if len(self.ordered_dict) > self.capacity:
+            oldest = next(iter(self.ordered_dict))
+            del self.ordered_dict[oldest]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+# @lc code=end
